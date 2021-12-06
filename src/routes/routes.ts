0b9fa409d44b2bb4380902ee659a88b26c2caa45
Alejoboga20 <alejoboga20@gmail.com>
2021-12-06 10:05:47 -0300

    Renaming chunks with webpack

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,14 @@
 import { lazy, LazyExoticComponent } from 'react';
 
-const LazyPage1 = lazy(() => import('../01-lazyload/pages/LazyPage1'));
-const LazyPage2 = lazy(() => import('../01-lazyload/pages/LazyPage2'));
-const LazyPage3 = lazy(() => import('../01-lazyload/pages/LazyPage3'));
+const LazyPage1 = lazy(
+	() => import(/* webpackChunkName: "LazyPage1" */ '../01-lazyload/pages/LazyPage1')
+);
+const LazyPage2 = lazy(
+	() => import(/* webpackChunkName: "LazyPage2" */ '../01-lazyload/pages/LazyPage2')
+);
+const LazyPage3 = lazy(
+	() => import(/* webpackChunkName: "LazyPage3" */ '../01-lazyload/pages/LazyPage3')
+);
 
 type JSXComponent = () => JSX.Element;
 
